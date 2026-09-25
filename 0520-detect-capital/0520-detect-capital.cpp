42b98dfd60732660faab40721726b5e8bb0ef9e4
Shubham Tripathi <95673479+23ShubhamCodes@users.noreply.github.com>
2026-09-25 22:49:15 +0530

    Time: 0 ms (100%), Space: 8.2 MB (49.94%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) 
+    {
+        if(word.size()==1)
+        {
+            return(true);
+        }
+        if(isupper(word[0]) && isupper(word[1]))
+        {
+            for(int i=2;i<word.size();i++)
+            {
+                if(islower(word[i]))
+                {
+                    return(false);
+                }
+            }
+        }
+        else if(isupper(word[0]) && islower(word[1]))
+        {
+            for(int i=2;i<word.size();i++)
+            {
+                if(isupper(word[i]))
+                {
+                    return(false);
+                }
+            }
+        }
+        else
+        {
+            for(int i=1;i<word.size();i++)
+            {
+                if(isupper(word[i]))
+                {
+                    return(false);
+                }
+            }
+        }
+        return(true);
+    }
+};
